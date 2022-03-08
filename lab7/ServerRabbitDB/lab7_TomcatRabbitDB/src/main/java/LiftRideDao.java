ecce4f20601de13e210a00ac2a536c71839c9aaa
fjprobos <probostecardenas.f@northeastern.edu>
2022-03-07 23:49:40 -0800

    Subject: Lab 7 partially finished

---
+++ lab7/ServerRabbitDB/lab7_TomcatRabbitDB/src/main/java/LiftRideDao.java
@@ -0,0 +1,43 @@
+import java.sql.*;
+import org.apache.commons.dbcp2.*;
+
+public class LiftRideDao {
+    private static BasicDataSource dataSource;
+
+    public LiftRideDao() {
+        dataSource = DBCPDataSource.getDataSource();
+    }
+
+    public void createLiftRide(LiftRide newLiftRide) {
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+        String insertQueryStatement = "INSERT INTO LiftRides (skierId, resortId, seasonId, dayId, time, liftId) " +
+                "VALUES (?,?,?,?,?,?)";
+        try {
+            conn = dataSource.getConnection();
+            preparedStatement = conn.prepareStatement(insertQueryStatement);
+            preparedStatement.setInt(1, newLiftRide.getSkierID());
+            preparedStatement.setInt(2, newLiftRide.getResortID());
+            preparedStatement.setString(3, Integer.toString(newLiftRide.getSeasonID()));
+            preparedStatement.setString(4, Integer.toString(newLiftRide.getDayID()));
+            preparedStatement.setInt(5, newLiftRide.getWaitTime());
+            preparedStatement.setInt(6, newLiftRide.getLiftID());
+
+            // execute insert SQL statement
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+}
